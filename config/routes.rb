3a0b6c8475b23583c752661a05a3e2b727c2155e
Jasdeep Garcha <jasdeepsgarcha@gmail.com>
2012-11-13 13:46:56 -0800

    v1 - user auth

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ A98lumens::Application.routes.draw do
 
   match 'about', :to=> "static_pages#about", :as => "about"
   match 'contact', :to=> "static_pages#contact", :as => "contact"
+  match 'projects', :to=> "static_pages#projects", :as => "projects"
   match 'test', :to=> "static_pages#test", :as => "test"
   match 'learn', :to=> "static_pages#learn", :as => "learn"
   match 'faqs', :to=> "static_pages#faqs", :as => "faqs"
